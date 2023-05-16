71a28bf9df073e92168e3a3ba89d071cc93b3e42
Danilo Santilli <danilo-santilli@hotmail.com>
2023-05-16 13:22:59 +0100

    Criando a página Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,47 @@
+import { useState } from 'react';
+import './home.css';
+import { Link } from 'react-router-dom'
+
 function Home() {
-    return (
-      <div>
-        <h1>Página Home</h1>
-      </div>
-    );
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleLogin(e){
+    e.preventDefault();
+    if (email !== '' && password !== '') {
+      alert('ok');
+    } else{
+      alert('No ok')
+    }
   }
   
-  export default Home;
+  return (
+    <div className='home-container'>
+      <h1>Lista de Tarefas</h1>
+      <span>Crie sua lista de forma fácil.</span>
+        
+      <form action="" className='form' onSubmit={handleLogin}>
+        <input 
+        type="text" 
+        placeholder='Digite seu email:'
+        value={email}
+        onChange={(e)=> setEmail(e.target.value)}/>
+        
+        <input 
+        type="password" 
+        placeholder='Digite sua senha:'
+        value={password}
+        onChange={(e)=> setPassword(e.target.value)}/>
+
+        <button type='submit'>Acessar</button>
+      </form>
+
+      <Link className='button-link' to='/register'>
+        Não possui uma conta? Crie uma agora!
+      </Link>
+    </div>
+  );
+}
+  
+export default Home;
